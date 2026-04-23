a4de344bd81c6deca56881a59efea4167c6e3f64
Vincent Munene <munenevincent49@gmail.com>
2026-04-23 23:50:24 +0300

    see product sales for a given period

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3567992460" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "512635325" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
